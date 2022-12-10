5717e4dcb7a551a653d1c3f00ffce63132dce964
v-teja <tejaviswa95@gmail.com>
2022-12-10 16:34:45 +0530

    sout removed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,8 +16,8 @@ public class Main {
 
     public static void main(String[] args) {
         Product p = new Product();
-        System.out.println(p.product(1,2));
-        System.out.println(p.product(1,2,3));
-        System.out.println(p.product(1.2,1.4));
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2,1.4);
     }
 }
